92ae12b890dcd38935ffcebbf6c00b27b9d2a92c
hbstarjason <hbstarjason@gmail.com>
2020-12-17 22:06:46 +0800

    Create 03run.sh

---
+++ 03run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#docker-compose up --build --force-recreate --detach jd3
+docker-compose up --no-build --force-recreate --detach jd3
+
+##京豆变动通知jd_bean_change.js
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_bean_change.js'
+
+##京东抽奖机jd_lotteryMachine.js  【东东抽奖机】、【新店福利】、【东东福利屋】、【东东生活】、【闪购盲盒】
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_lotteryMachine.js'
+
+##京东排行榜jd_rankingList.js
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_rankingList.js'
+
+##京小超jd_superMarket.js
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_superMarket.js'
+
+##京小超兑换奖品jd_blueCoin.js
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_blueCoin.js'
+
+## 东东小窝jd_small_home.js
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_small_home.js'
+
+## 东东工厂jd_jdfactory.js
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_jdfactory.js'
+
+## 健康抽奖机jd_health.js
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_health.js'
