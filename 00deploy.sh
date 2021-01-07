e0de075d74227a26f5768bbdbac980903367a3a2
hbstarjason <hbstarjason@gmail.com>
2021-01-07 22:20:20 +0800

    Update 00deploy.sh

--- 00deploy.sh
+++ 00deploy.sh
@@ -1,5 +1,6 @@
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)"  \
    -o /usr/local/bin/docker-compose  && \
    sudo chmod +x /usr/local/bin/docker-compose &&  docker-compose version && \
-   docker pull hbstarjason/jd-scripts && git clone https://github.com/hbstarjason/jd-scripts-docker/ && \
-   cd jd-scripts-docker && ls -l 
+    git clone https://github.com/hbstarjason/jd-scripts-docker/ && \
+   cd jd-scripts-docker && ls -l  && \
+ docker pull hbstarjason/jd-scripts
