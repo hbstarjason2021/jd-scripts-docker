d406c0161d8172661280e2d0dbc2fe158326b91d
hbstarjason <hbstarjason@gmail.com>
2021-01-09 19:18:42 +0800

    Update 00deploy.sh

--- 00deploy.sh
+++ 00deploy.sh
@@ -5,5 +5,6 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-
    cd jd-scripts-docker && ls -l  && \
  docker pull hbstarjason/jd-scripts
 
+docker-compose up --no-build --force-recreate --detach jd1
 
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_crazy_joy_coin.js'
