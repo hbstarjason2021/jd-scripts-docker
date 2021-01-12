07f8c5347e15b77eded7ce6035c48c8c9dc825da
hbstarjason <hbstarjason@gmail.com>
2021-01-12 22:31:41 +0800

    Update 00deploy.sh

--- 00deploy.sh
+++ 00deploy.sh
@@ -9,8 +9,8 @@ sudo curl -L "http://rancher-mirror.cnrancher.com/docker-compose/v1.27.4/docker-
  docker pull hbstarjason/jd-scripts
 
 docker-compose up --no-build --force-recreate --detach jd1
-
 docker-compose up --no-build --force-recreate --detach jd2
+docker-compose up --no-build --force-recreate --detach jd4
 
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_crazy_joy_coin.js'
 
@@ -18,3 +18,7 @@ docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-
 
 
 cp 01run.sh 02run.sh &&  sed -i "s/jd1/jd2/g"  02run.sh 
+cp 01run.sh 04run.sh &&  sed -i "s/jd1/jd2/g"  04run.sh
+
+sed -i '2s/pt_key=/XXXX/' env/env1 && \
+  sed -i '3s/pt_pin=/pt_pin=jd_709c349f13b51/' env/env1
