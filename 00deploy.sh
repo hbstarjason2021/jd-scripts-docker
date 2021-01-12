a950a18356453d9f3bb1c17bd2ac2affabf4833a
hbstarjason <hbstarjason@gmail.com>
2021-01-12 18:04:49 +0800

    Update 00deploy.sh

--- 00deploy.sh
+++ 00deploy.sh
@@ -14,4 +14,7 @@ docker-compose up --no-build --force-recreate --detach jd2
 
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_crazy_joy_coin.js'
 
+docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_bean_change.js'
+
+
 cp 01run.sh 02run.sh &&  sed -i "s/jd1/jd2/g"  02run.sh 
