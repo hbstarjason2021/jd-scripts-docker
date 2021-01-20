461ae33afb282ae86ca73b89761e226af869a5d7
hbstarjason <hbstarjason@gmail.com>
2021-01-20 11:36:45 +0800

    Update 00deploy.sh

--- 00deploy.sh
+++ 00deploy.sh
@@ -27,3 +27,6 @@ sed -i '2s/pt_key=/XXXX/' env/env1 && \
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node getJDCookie.js'
 
 docker cp jd_ms.js jd1:/scripts/jd_ms.js
+docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_ms.js'
+
+docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_joy.js'
