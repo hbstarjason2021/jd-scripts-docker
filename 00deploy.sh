a1caeaf7dc67fcc2363bfae8939566590d73a3c8
hbstarjason <hbstarjason@gmail.com>
2021-01-21 00:26:51 +0800

    Update 00deploy.sh

--- 00deploy.sh
+++ 00deploy.sh
@@ -30,3 +30,5 @@ docker cp jd_ms.js jd1:/scripts/jd_ms.js
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_ms.js'
 
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_joy.js'
+docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_jdzz.js'
+
