c604ccac06b4ba4b65a0973d3625438e93c9ba20
hbstarjason <hbstarjason@gmail.com>
2021-01-21 23:32:24 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -3,9 +3,9 @@
 */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  'pt_key=XXX;pt_pin=XXX;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
-  'pt_key=XXX;pt_pin=XXX;',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
-  'pt_key=XXX;pt_pin=XXX;',
+  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
+  '',
 ]
 // 判断环境变量里面是否有京东ck
 if (process.env.JD_COOKIE) {
