9936991a4b83258581e594dbca94b8647d70b96c
hbstarjason <hbstarjason@gmail.com>
2021-03-23 17:50:56 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -104,17 +104,20 @@ cp /jd-scripts-docker/from/jx_cfd_exchange.js /scripts/jx_cfd_exchange.js
 cp /jd-scripts-docker/from/jx_cfdtx.js /scripts/jx_cfdtx.js
 cp /jd-scripts-docker/from/jdJxncTokens.js /scripts/jdJxncTokens.js
 
+cp /jd-scripts-docker/from/jd_daydlt.js /scripts/jd_daydlt.js
+cp /jd-scripts-docker/from/jdautogan.js /scripts/jdautogan.js
+
+
 ##cp /jd-scripts-docker/from/jd_entertainment.js /scripts/jd_entertainment.js
 cp /qx/jd_entertainment.js /scripts/jd_entertainment.js && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/jd_entertainment.js
 ##cp /jd-scripts-docker/from/jd_fanslove.js /scripts/jd_fanslove.js
 cp /qx/jd_fanslove.js /scripts/jd_fanslove.js && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/jd_fanslove.js
-
 ##cp /jd-scripts-docker/from/z_wish.js  /scripts/z_wish.js 
 cp /qx/z_wish.js  /scripts/z_wish.js && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/z_wish.js
 
-cp /qx/z_lenovo.js  /scripts/z_lenovo.js && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/z_lenovo.js
-cp /qx/z_mgold.js  /scripts/z_mgold.js && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/z_mgold.js
-cp /qx/z_super5g.js  /scripts/z_super5g.js && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/z_super5g.js
+cp /qx/z_lenovo.js     /scripts/z_lenovo.js    && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/z_lenovo.js
+cp /qx/z_mgold.js      /scripts/z_mgold.js     && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/z_mgold.js
+cp /qx/z_super5g.js    /scripts/z_super5g.js   && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/z_super5g.js
 cp /qx/z_city_cash.js  /scripts/z_city_cash.js && sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/z_city_cash.js
 
 cp /jd-scripts-docker/from/jd_mlyjy.js /scripts/jd_mlyjy.js
