09da29bf6f8198643521e7322570b99a6f6e4eb2
hbstarjason <hbstarjason@gmail.com>
2021-04-02 02:26:10 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -70,7 +70,8 @@ trap 'cp /jd-scripts-docker/sync.sh /sync' Exit
   cd /scripts-bak
   git pull
 ) || {
-  git clone --branch=master https://gitlab.com/MrRight/Scripts.git /scripts-bak_tmp
+  #git clone --branch=master https://gitlab.com/MrRight/Scripts.git /scripts-bak_tmp
+  git clone --branch=dust https://github.com/hbstarjason/jd_scripts.git /scripts-bak_tmp
   [ -d /scripts-bak_tmp ] && {
     rm -rf /scripts-bak
     mv /scripts-bak_tmp /scripts-bak
