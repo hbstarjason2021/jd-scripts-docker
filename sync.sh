b633a38128570634ca85dac4c9b00193b78420ed
hbstarjason <hbstarjason@gmail.com>
2021-04-26 02:00:40 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -151,6 +151,7 @@ cp /jd-scripts-docker/from/jd_jxcfd.js /scripts/jd_jxcfd.js
 #cp /jd-scripts-docker/jdCookie.js /Loon
 
 #cd /Loon && npm install -g
+npm install request
 
 ##################################
 sed -i 's/helpAuthor = true/helpAuthor = false/' /scripts/jd_cash.js
