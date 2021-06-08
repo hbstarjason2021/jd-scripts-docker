69677ecef3a4f9c26fcd4699e1fcfbc543630d02
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-06-08 10:08:40 +0800

    Update ddo_pk.js

--- from/ddo_pk.js
+++ from/ddo_pk.js
@@ -1,7 +1,7 @@
 /*
 京享值PK
 cron 15 0,6,13,19,21 * * * ddo_pk.js
-更新时间：2021-6-4
+更新时间：2021-6-7
 活动入口：京东APP-我的-京享值
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
@@ -189,7 +189,7 @@ function getPinList(num = 20){
 			try {
 				if (res) {
 					let data = $.toObj(res);
-					$.pinList = data.data.split(',');
+					$.pinList = data.data;
 				}
 			} catch (e) {
 				console.log(e);
