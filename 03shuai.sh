0305ca193d644c95a450619b4f5818a2d55a0194
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-09-10 10:24:12 +0800

    Create 03shuai.sh

---
+++ 03shuai.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#docker-compose up --build --force-recreate --detach jd3
+docker-compose up --no-build --force-recreate --detach jd3
+
+##京豆变动通知jd_bean_change.js
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_bean_change.js'
+
+##东东农场
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_fruit.js'
+
+##种豆得豆
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_plantBean.js'
+
+##东东萌宠
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_pet.js'
+
+##闪购盲盒
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_sgmh.js'
+
+##集卡
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_jika.js'
+
+##财富岛助力
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node gua_wealth_island_help.js'
+
+##内容鉴赏
+docker exec jd3 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_connoisseur.js'
