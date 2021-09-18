821c9b4fede19e6b4853dd22e34f9bd4e9f0ee3f
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-09-18 17:26:43 +0800

    Update jd_ddworld-new.js

--- from/jd_ddworld-new.js
+++ from/jd_ddworld-new.js
@@ -67,11 +67,11 @@ let tokenInfo = {}, hotInfo = {}
       hotInfo[$.UserName] = $.hot
     }
   }
-  let res = await getAuthorShareCode('')
+  let res = await getAuthorShareCode('https://gitee.com/starjason/sharecode/raw/master/ddworld.json')
   if (!res) {
-    $.http.get({url: ''}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
+    $.http.get({url: 'https://gitee.com/starjason/sharecode/raw/master/ddworld.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
-    res = await getAuthorShareCode('')
+    res = await getAuthorShareCode('https://gitee.com/starjason/sharecode/raw/master/ddworld.json')
   }
   $.shareCodes = [...$.shareCodes, ...(res || [])]
   for (let i = 0; i < cookiesArr.length; i++) {
