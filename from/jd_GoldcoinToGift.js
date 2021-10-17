17b01666ed5d68768b4210a320658c101855a289
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-10-17 16:49:15 +0800

    Update jd_GoldcoinToGift.js

--- from/jd_GoldcoinToGift.js
+++ from/jd_GoldcoinToGift.js
@@ -63,11 +63,11 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
       await GoldcoinToGift()
     }
   }
-  let res = await getAuthorShareCode('')
+  let res = await getAuthorShareCode('https://gitee.com/starjason/sharecode/raw/master/GoldcoinToGift.json')
   if (!res) {
-    $.http.get({url: ''}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
+    $.http.get({url: 'https://gitee.com/starjason/sharecode/raw/master/GoldcoinToGift.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
-    res = await getAuthorShareCode('')
+    res = await getAuthorShareCode('https://gitee.com/starjason/sharecode/raw/master/GoldcoinToGift.json')
   }
   $.shareId = [...new Set([...$.shareId, ...(res || [])])]
   for (let i = 0; i < cookiesArr.length; i++) {
