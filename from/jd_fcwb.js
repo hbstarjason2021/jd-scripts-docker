963f8f44c9c41aa87c6aaacafdbb64debc047a10
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-11-08 13:25:19 +0800

    Update jd_fcwb.js

--- from/jd_fcwb.js
+++ from/jd_fcwb.js
@@ -33,9 +33,9 @@ let fcwbinviteCode = "";
     }
     console.log(`\n注意：本脚本暂时只会执行助力，助力后，请手动进活动进行游戏（发财挖宝: 入口,极速版-》我的-》发财挖宝）\n`)
     let res = [];
-    try{res = await getAuthorShareCode('');}catch (e) {}
+    try{res = await getAuthorShareCode('https://gitee.com/starjason/sharecode/raw/master/fcwb.json');}catch (e) {}
     if(!res){
-        try{res = await getAuthorShareCode('');}catch (e) {}
+        try{res = await getAuthorShareCode('https://gitee.com/starjason/sharecode/raw/master/fcwb.json');}catch (e) {}
         if(!res){res = [];}
     }
     if(res.length > 0){
