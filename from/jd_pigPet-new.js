7594e2e6941ca8c9711bc965547444c9d0eabcc0
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-11-20 11:33:28 +0800

    Update jd_pigPet-new.js

--- from/jd_pigPet-new.js
+++ from/jd_pigPet-new.js
@@ -27,8 +27,7 @@ const MISSION_BASE_API = `https://ms.jr.jd.com/gw/generic/mission/h5/m`;
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let shareId = [""][Math.floor((Math.random() * 4))];
-let helpId = ["", ""];
+let shareId = ["", "", "", ""][Math.floor((Math.random() * 4))];
 $.shareCodes = [];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
@@ -44,7 +43,8 @@ if ($.isNode()) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
     return;
   }
-  console.log(`\n【原作者：LXK大佬】\n\nBy：zero205\n添加：邀请新用户，大转盘助力\n修改：优化日志输出，自动喂食\nTodo：抢粮食`);
+  await getShareCode();
+  console.log(`\n【原作者：LXK大佬】\n\nBy：zero205\n添加：邀请新用户，大转盘助力，抢粮食\n修改：优化日志输出，自动喂食\n\n默认不抢粮食（成功机率小），需要的请添加变量JD_PIGPET_PK，值填true\n`);
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -66,7 +66,7 @@ if ($.isNode()) {
     }
   }
   console.log(`\n======开始大转盘助力======\n`);
-  $.shareCodes = [...$.shareCodes, ...helpId]
+  $.shareCodes = [...$.shareCodes, ...($.helpId || [])]
   for (let j = 0; j < cookiesArr.length; j++) {
     cookie = cookiesArr[j];
     if ($.shareCodes && $.shareCodes.length) {
@@ -78,17 +78,17 @@ if ($.isNode()) {
       }
     }
   }
-  if (allMessage && new Date().getHours() % 6 === 0) {
+  if (process.env.PIGNF != 'false' && allMessage && new Date().getHours() % 6 === 0) {
     if ($.isNode()) await notify.sendNotify($.name, allMessage);
     $.msg($.name, '', allMessage);
   }
 })()
-  .catch((e) => {
-    $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
-  })
-  .finally(() => {
-    $.done();
-  })
+    .catch((e) => {
+      $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
+    })
+    .finally(() => {
+      $.done();
+    })
 async function jdPigPet() {
   try {
     await pigPetLogin();
@@ -98,6 +98,9 @@ async function jdPigPet() {
     await pigPetOpenBox();
     await pigPetLotteryIndex();
     await pigPetLottery();
+    if (process.env.JD_PIGPET_PK && process.env.JD_PIGPET_PK === 'true') {
+      await pigPetRank();
+    }
     await pigPetMissionList();
     await missions();
     if ($.finish) return
@@ -192,6 +195,7 @@ function pigPetUserBag() {
                         item.count = item.count - 20
                         i--
                       } while (item.count >= 20 && i > 0)
+                      if ($.finish) break
                     }
                   }
                 } else {
@@ -382,6 +386,132 @@ function pigPetLotteryIndex() {
     })
   })
 }
+//查询排行榜好友
+function pigPetRank() {
+  return new Promise(async resolve => {
+    const body = {
+      "type": 1,
+      "page": 1,
+      "source": 2,
+      "channelLV": "juheye",
+      "riskDeviceParam": "{}"
+    }
+    $.post(taskUrl('pigPetRank', body), async (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} pigPetRank API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            n = 0
+            if (data.resultCode === 0) {
+              if (data.resultData.resultCode === 0 && n < 5) {
+                $.friends = data.resultData.resultData.friends
+                for (let i = 0; i < $.friends.length; i++) {
+                  if ($.friends[i].status === 1) {
+                    $.friendId = $.friends[i].uid
+                    $.name = $.friends[i].nickName
+                    if (!['zero205', 'xfa05'].includes($.name)) { //放过孩子吧TT
+                      console.log(`去抢夺【${$.friends[i].nickName}】的食物`)
+                      await $.wait(2000)
+                      await pigPetFriendIndex($.friendId)
+                    }
+                  }
+                }
+              } else {
+                console.log(`查询排行榜失败：${JSON.stringify(data)}`)
+              }
+            }
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+function pigPetFriendIndex(friendId) {
+  return new Promise(async resolve => {
+    const body = {
+      "friendId": friendId,
+      "source": 2,
+      "channelLV": "juheye",
+      "riskDeviceParam": "{}"
+    }
+    $.post(taskUrl('pigPetFriendIndex', body), async (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} pigPetFriendIndex API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            if (data.resultCode === 0) {
+              if (data.resultData.resultCode === 0) {
+                await pigPetRobFood($.friendId)
+                await $.wait(3000)
+              } else {
+                console.log(`进入好友猪窝失败：${JSON.stringify(data)}`)
+              }
+            }
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+//抢夺食物
+async function pigPetRobFood(friendId) {
+  return new Promise(async resolve => {
+    const body = {
+      "source": 2,
+      "friendId": friendId,
+      "channelLV": "juheye",
+      "riskDeviceParam": "{}"
+    }
+    $.post(taskUrl('pigPetRobFood', body), (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            if (data.resultCode === 0) {
+              if (data.resultData.resultCode === 0) {
+                if (data.resultData.resultData.robFoodCount > 0) {
+                  console.log(`抢夺成功，获得${data.resultData.resultData.robFoodCount}g${data.resultData.resultData.robFoodName}\n`);
+                  n++
+                } else {
+                  console.log(`抢夺失败，损失${data.resultData.resultData.robFoodCount}g${data.resultData.resultData.robFoodName}\n`);
+                }
+              } else {
+                console.log(`抢夺失败：${JSON.stringify(data)}\n`)
+              }
+            }
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
 //查询签到情况
 function pigPetSignIndex() {
   $.sign = true;
@@ -444,9 +574,9 @@ function pigPetLotteryPlay() {
               if (data.resultData.resultCode === 0) {
                 if (data.resultData.resultData) {
                   if (data.resultData.resultData.award) {
-                    console.log(`大转盘抽奖获得：${data.resultData.resultData.award.name} * ${data.resultData.resultData.award.count}`);
+                    console.log(`大转盘抽奖获得：${data.resultData.resultData.award.name} * ${data.resultData.resultData.award.count}\n`);
                   } else {
-                    console.log(`大转盘抽奖结果：没抽中，再接再励哦～`)
+                    console.log(`大转盘抽奖结果：没抽中，再接再励哦～\n`)
                   }
                   $.currentCount = data.resultData.resultData.currentCount;//抽奖后剩余的抽奖次数
                 }
@@ -733,6 +863,32 @@ function finishReadMission(missionId, readTime) {
     })
   })
 }
+
+function getShareCode() {
+  return new Promise(resolve => {
+    $.get({
+      //url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/pig.json",
+      url: "",
+      headers: {
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
+      }
+    }, async (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`);
+          console.log(`${$.name} API请求失败，请检查网路重试`);
+        } else {
+          $.helpId = JSON.parse(data);
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+
 function TotalBean() {
   return new Promise(async resolve => {
     const options = {
