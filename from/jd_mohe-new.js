76b3be2a5b9bb9cfa173d23df24b7b873f7264c2
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-11-21 11:52:08 +0800

    Update jd_mohe-new.js

--- from/jd_mohe-new.js
+++ from/jd_mohe-new.js
@@ -449,7 +449,7 @@ function updateShareCodesCDN(url) {
 function getShareCode() {
   return new Promise(resolve => {
     $.get({
-      url: "",
+      url: "https://gitee.com/starjason/sharecode/raw/master/jd_mohe.json",
       headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }
