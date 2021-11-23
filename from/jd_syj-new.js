8b567fd517a4c8a1bc53d85154a43be90a235a79
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-11-23 18:11:44 +0800

    Update jd_syj-new.js

--- from/jd_syj-new.js
+++ from/jd_syj-new.js
@@ -731,7 +731,8 @@ function getAuthorShareCode(url) {
   })
 }
 async function getRandomCode() {
-  await $.http.get({url: `http://go.chiang.fun/read/zuan/${randomCount}`, timeout: 10000}).then(async (resp) => {
+  //await $.http.get({url: `http://go.chiang.fun/read/zuan/${randomCount}`, timeout: 10000}).then(async (resp) => {
+  await $.http.get({url: ``, timeout: 10000}).then(async (resp) => {  
     if (resp.statusCode === 200) {
       try {
         let { body } = resp;
