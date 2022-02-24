d31f39ace09e3b170cc26a4202881733f0947585
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-24 18:34:00 +0800

    Update jd_joypark_joy.js

--- from/jd_joypark_joy.js
+++ from/jd_joypark_joy.js
@@ -512,7 +512,7 @@ function apCashWithDraw(id, poolBaseId, prizeGroupId, prizeBaseId) {
 function getShareCode() {
   return new Promise(resolve => {
       $.get({
-          url: "https://raw.githubusercontent.com/KingRan/JD-Scripts/main/shareCodes/joypark.json",
+          url: "",
           headers: {
               "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
           }
