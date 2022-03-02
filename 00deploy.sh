5e3738176ee074f5cfbd2a9191c9852a1360a1c2
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-03-02 15:15:26 +0800

    Update 00deploy.sh

--- 00deploy.sh
+++ 00deploy.sh
@@ -22,14 +22,6 @@ cp 01run.sh 04run.sh &&  sed -i "s/jd1/jd4/g"  04run.sh
 
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_bean_change.js'
 
-docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_crazy_joy_coin.js'
-docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_speed_sign.js'
-
-docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jdautogan.js'
-
-docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jx_cash_sign.js'
-
-
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /jddj; node jddj_bean.js'
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /jddj; node jddj_plantBeans.js'
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /jddj; node jddj_getPoints.js'
@@ -42,9 +34,21 @@ docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-
  docker exec jd4 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_plantBean.js'
  
 
-
 sed -i '2s/pt_key=/XXXX/' env/env1 && \
   sed -i '3s/pt_pin=/pt_pin=jd_709c349f13b51/' env/env1
   
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node getJDCookie.js'
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_get_share_code.js'
+
+<< CONTENT
+
+https://www.katacoda.com/courses/kubernetes/launch-single-node-cluster
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)"  \
+   -o /usr/local/bin/docker-compose  && \
+   sudo chmod +x /usr/local/bin/docker-compose &&  docker-compose version && \
+    git clone https://github.com/hbstarjason2021/jd-scripts-docker && \
+   cd jd-scripts-docker && ls -l  && \
+ docker pull hbstarjason/jd-scripts
+
+CONTENT
