9ac2383393d0416c4d220b644b17c00550fe2069
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-03-02 15:22:37 +0800

    Update 00deploy.sh

--- 00deploy.sh
+++ 00deploy.sh
@@ -44,7 +44,7 @@ docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-
 
 https://www.katacoda.com/courses/kubernetes/launch-single-node-cluster
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)"  \
+sudo curl -L "https://github.com/docker/compose/releases/download/2.2.2/docker-compose-$(uname -s)-$(uname -m)"  \
    -o /usr/local/bin/docker-compose  && \
    sudo chmod +x /usr/local/bin/docker-compose &&  docker-compose version && \
     git clone https://github.com/hbstarjason2021/jd-scripts-docker && \
