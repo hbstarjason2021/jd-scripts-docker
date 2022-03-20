ca9c1ae55937e51e93777a31c9e81228c92d18de
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-03-20 22:43:20 +0800

    Update jd_daily_lottery.js

--- from/jd_daily_lottery.js
+++ from/jd_daily_lottery.js
@@ -2,7 +2,20 @@
 小鸽有礼 - 每日抽奖
 活动入口：京东首页搜索 边玩边赚
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
-cron "21 1,19 * * *" jd_daily_lottery.js
+===================quantumultx================
+[task_local]
+#每日抽奖
+13 1,22,23 * * * jd_daily_lottery.js, tag=每日抽奖, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+
+=====================Loon================
+[Script]
+cron "13 1,22,23 * * *" script-path=jd_daily_lottery.js, tag=每日抽奖
+
+====================Surge================
+每日抽奖 = type=cron,cronexp="13 1,22,23 * * *",wake-system=1,timeout=3600,script-path=jd_daily_lottery.js
+
+============小火箭=========
+每日抽奖 = type=cron,script-path=jd_daily_lottery.js, cronexpr="13 1,22,23 * * *", timeout=3600, enable=true
 */
 const $ = new Env('小鸽有礼-每日抽奖');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -10,7 +23,8 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let activityType = '';
 let activityCode = '';
 const activityInfoList = [
-  {'activityType':'WonderfulLuckDrawApi','activityCode':'1410048365793640448','title':'小哥有礼'}
+  {'activityType':'WonderfulLuckDrawApi','activityCode':'1410048365793640448','title':'小哥有礼'},
+  {'activityType':'luckdraw','activityCode':'1493527662965030912','title':'每日转盘'}
 ];
 $.helpCodeList = [];
 //IOS等用户直接用NobyDa的jd cookie
@@ -239,9 +253,9 @@ function dealReturn(functionId, data) {
         $.helpCodeList.push({
           'use': $.UserName,
           'helpCpde': data.data,
-          'needHelp': missionInfo['totalNum'] - missionInfo['completeNum']
+          //'needHelp': missionInfo['totalNum'] - missionInfo['completeNum']
         });
-        console.log(`互助码(内部多账号自己互助)：${data.data}`);
+        //console.log(`互助码(内部多账号自己互助)：${data.data}`);
       }
       break;
     case 'helpFriend':
