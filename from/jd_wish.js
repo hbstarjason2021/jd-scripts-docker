3cdab77b1ff524ab4b7dfd9d382f2b52d34b5447
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-03-22 15:02:15 +0800

    Update jd_wish.js

--- from/jd_wish.js
+++ from/jd_wish.js
@@ -25,8 +25,8 @@ let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ['1EFZWxKqP', "1EFRWxKuG", "1E1xZy6s", "1EFRQwA", "1FFVQyqw", '1EFRWxKuG'];
-let appNameArr = ['新品来袭', "许愿抽好礼", "PLUS生活特权", "疯狂砸金蛋", "1111点心动", '众筹许愿池'];
+let appIdArr = ['1EFdRwqyF','1EFZWxKqP', "1EFRWxKuG", "1E1xZy6s", "1EFRQwA", "1FFVQyqw", '1EFRWxKuG'];
+let appNameArr = ['春尚','新品来袭', "许愿抽好礼", "PLUS生活特权", "疯狂砸金蛋", "1111点心动", '众筹许愿池'];
 let appId, appName;
 $.shareCode = [];
 if ($.isNode()) {
