a144763be95c0ae5b2dce98ade38f9091dce955c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-04-11 19:06:37 +0800

    Update jd_carnivalcity_help.js

--- from/jd_carnivalcity_help.js
+++ from/jd_carnivalcity_help.js
@@ -253,7 +253,7 @@ function getAuthorShareCode(url) {
 
 function readShareCode() {
   return new Promise(async resolve => {
-    $.get({url: `https://gitee.com/starjason/sharecode/raw/master/carnivalcity`, 'timeout': 20000}, (err, resp, data) => {
+    $.get({url: `https://gitee.com/starjason/sharecode/raw/master/carnivalcity2`, 'timeout': 20000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
