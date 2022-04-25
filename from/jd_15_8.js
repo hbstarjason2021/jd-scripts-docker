225c5c9231ea9450906a7a404b55ac2abeb74e39
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-04-25 15:55:07 +0800

    Update jd_15_8.js

--- from/jd_15_8.js
+++ from/jd_15_8.js
@@ -20,7 +20,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
-const randomCount = $.isNode() ? 30 : 5;
+const randomCount = $.isNode() ? 15 : 5;
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 if ($.isNode()) {
@@ -79,7 +79,7 @@ function exchange() {
             data = JSON.parse(data);
             console.log(`抢券结果：${JSON.stringify(data)}\n`)
             if ("A1" === data.subCode)  {
-              notify.sendNotify(`${$.name}`, `【京东账号${$.index}】${$.nickName}\n抢到15-8券了`)
+              notify.sendNotify(`${$.name}`, `【京东账号${$.index}】${$.UserName}\n抢到15-8券了`)
             }
           }
         }
