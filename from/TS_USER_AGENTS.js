acddd76281eda5eb148ad400919cd412b2601778
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-24 11:47:09 +0800

    Update TS_USER_AGENTS.js

--- from/TS_USER_AGENTS.js
+++ from/TS_USER_AGENTS.js
@@ -370,7 +370,7 @@ function getshareCodeHW(key) {
                     _a.label = 2;
                 case 2:
                     _a.trys.push([2, 4, , 6]);
-                    return [4 /*yield*/, axios_1["default"].get('https://gitee.com/starjason/sharecode/raw/master/HW_CODES')];
+                    return [4 /*yield*/, axios_1["default"].get('https://jihulab.com/hbstarjason/sharecode/-/raw/master/HW_CODES')];
                 case 3:
                     data = (_a.sent()).data;
                     shareCodeHW = data[key] || [];
@@ -408,7 +408,7 @@ function getShareCodePool(key, num) {
                     _a.label = 2;
                 case 2:
                     _a.trys.push([2, 4, , 6]);
-                    return [4 /*yield*/, axios_1["default"].get("https://gitee.com/starjason/sharecode/raw/master/HW_CODES")];
+                    return [4 /*yield*/, axios_1["default"].get("https://jihulab.com/hbstarjason/sharecode/-/raw/master/HW_CODES")];
                 case 3:
                     data = (_a.sent()).data;
                     shareCode = data.data || [];
