a7a14811c8844388a3f56ba997808419136352ff
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-24 11:54:59 +0800

    Update jx_nnfls-new.js

--- from/jx_nnfls-new.js
+++ from/jx_nnfls-new.js
@@ -93,7 +93,7 @@ if ($.isNode()) {
 function getShareCode(name, author = 'starjason', num = -1, shuffle = false) {
     return new Promise(resolve => {
         $.get({
-            url: `https://gitee.com/${author}/sharecode/raw/master/${name}`,
+            url: `https://jihulab.com/hbstarjason/sharecode/-/raw/master/nnfls.json}`,
             headers: {
                 "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
             }
