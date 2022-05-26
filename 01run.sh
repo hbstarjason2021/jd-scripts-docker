8124a7d1736ad080936e6cc032abd33b21596898
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-26 12:38:44 +0800

    Update 01run.sh

--- 01run.sh
+++ 01run.sh
@@ -42,6 +42,9 @@ docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-
 ### 特务Z预约
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_mx_tewuz_booking.js'
 
+### 探味奇遇记
+docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_tanwei.js'
+
 ## 京享周周乐
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_xs_zzl.js'
 
