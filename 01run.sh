068fe3fe8a1bf5ae10759d44144c314240abf99f
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-06-18 22:23:04 +0800

    Update 01run.sh

--- 01run.sh
+++ 01run.sh
@@ -49,8 +49,6 @@ docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-
 ### 探味奇遇记
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_tanwei.js'
 
-### 小米618
-docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_xm618.js'
 
 ## 京享周周乐
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_xs_zzl.js'
@@ -221,6 +219,9 @@ docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-
 
 
 #################################################################################################################################################
+
+### 小米618
+#docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_xm618.js'
 
 ## 内容鉴赏官
 #docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_connoisseur.js'
