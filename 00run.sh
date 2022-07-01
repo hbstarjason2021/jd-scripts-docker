a9cec7ebf8d31893251d79841905a7c714dfc686
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-07-01 22:20:12 +0800

    Update 00run.sh

--- 00run.sh
+++ 00run.sh
@@ -2,15 +2,15 @@
 
 #############################################################################################################################
 
-curl "https://api.jdsharecode.xyz/api/runTimes0509?activityId=farm&sharecode=ab20ce7ec4294af6af5f11ce657e6bcb"
-curl "https://api.jdsharecode.xyz/api/runTimes0509?activityId=farm&sharecode=2764c0d2f0c44a32906c895b3cca8c66"
-curl "https://api.jdsharecode.xyz/api/runTimes0509?activityId=farm&sharecode=0fda5c9ebd194434aaecbbdf0d8741af"
-curl "https://api.jdsharecode.xyz/api/runTimes0509?activityId=farm&sharecode=670b6a89e4694fc3be6b79bc93a51c4e"
+curl "https://api.jdsharecode.xyz/api/runTimes0701?activityId=farm&sharecode=ab20ce7ec4294af6af5f11ce657e6bcb"
+curl "https://api.jdsharecode.xyz/api/runTimes0701?activityId=farm&sharecode=2764c0d2f0c44a32906c895b3cca8c66"
+curl "https://api.jdsharecode.xyz/api/runTimes0701?activityId=farm&sharecode=0fda5c9ebd194434aaecbbdf0d8741af"
+curl "https://api.jdsharecode.xyz/api/runTimes0701?activityId=farm&sharecode=670b6a89e4694fc3be6b79bc93a51c4e"
 
-curl "https://api.jdsharecode.xyz/api/runTimes0509?activityId=bean&sharecode=4npkonnsy7xi23qpldd3r2j7uacjaa2lz2pdlji"
-curl "https://api.jdsharecode.xyz/api/runTimes0509?activityId=bean&sharecode=ik7t7zkjpb2rs6zc7ufixlk6xa"
-curl "https://api.jdsharecode.xyz/api/runTimes0509?activityId=bean&sharecode=ll3bia54pjfx47gq5c2rmder7u"
-curl "https://api.jdsharecode.xyz/api/runTimes0509?activityId=bean&sharecode=5y5ua2n2xjibrnna4xpkq3gmmq"
+curl "https://api.jdsharecode.xyz/api/runTimes0701?activityId=bean&sharecode=4npkonnsy7xi23qpldd3r2j7uacjaa2lz2pdlji"
+curl "https://api.jdsharecode.xyz/api/runTimes0701?activityId=bean&sharecode=ik7t7zkjpb2rs6zc7ufixlk6xa"
+curl "https://api.jdsharecode.xyz/api/runTimes0701?activityId=bean&sharecode=ll3bia54pjfx47gq5c2rmder7u"
+curl "https://api.jdsharecode.xyz/api/runTimes0701?activityId=bean&sharecode=5y5ua2n2xjibrnna4xpkq3gmmq"
 
 ### api.jdsharecode.xyz
 docker exec jd4 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /JDHelloWorld; ts-node jd_cfd_up.ts'
