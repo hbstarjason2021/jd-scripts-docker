7a08f13a0955ae0a5826f34724c3ffab499db2f5
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-07-09 13:35:00 +0800

    Update 01run.sh

--- 01run.sh
+++ 01run.sh
@@ -18,6 +18,9 @@ docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-
 ## jd_club_lottery.js
 docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_club_lottery.js'
 
+## jd_ry.js
+docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_ry.js'
+
 ## 京东汽车jd_car.js 
 #docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_car.js'
 #docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_car_exchange.js'
