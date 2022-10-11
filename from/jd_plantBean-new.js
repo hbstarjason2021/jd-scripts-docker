575f6c56045b4e0dda7b79f15ee03b6da17fe608
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-10-12 00:51:58 +0800

    Update jd_plantBean-new.js

--- from/jd_plantBean-new.js
+++ from/jd_plantBean-new.js
@@ -28,7 +28,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //助力好友分享码(最多3个,否则后面的助力失败)
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
-let shareCodes = ['']
+let shareCodes = ['4npkonnsy7xi23qpldd3r2j7uacjaa2lz2pdlji@ik7t7zkjpb2rs6zc7ufixlk6xa@ll3bia54pjfx47gq5c2rmder7u@5y5ua2n2xjibrnna4xpkq3gmmq']
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
