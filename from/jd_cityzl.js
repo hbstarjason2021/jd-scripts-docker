6975d7669254ac2115adb1ac5d6e62a26349da81
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-12-09 22:02:13 +0800

    Update jd_cityzl.js

--- from/jd_cityzl.js
+++ from/jd_cityzl.js
@@ -26,7 +26,7 @@ if ($.isNode()) {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let inviteCodes = []
+let inviteCodes = ['46yqG_ZdJTsdL17EBpfUh4s','6a2zAP1fPjEdIkuIBtuJu_7yvQ','9q3-XK4MPD0dNF6ISpeJ5ire','-ryUXqwFNWdHakTFSYnA9sux6ZHZflFB']
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
