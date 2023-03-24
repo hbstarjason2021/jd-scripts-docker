93b3ca0509773fa043f3a9c0a34f1a33c407b941
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-03-24 22:26:20 +0800

    Update jd_joy_run_reward.ts

--- from/jd_joy_run_reward.ts
+++ from/jd_joy_run_reward.ts
@@ -1,13 +1,12 @@
 /**
-汪汪赛跑-提现10元
-3 0 0 * * 5 jd_joy_run_reward.ts
+汪汪赛跑-提现10元,周五9点
+59 59 8 * * 5 jd_joy_run_reward.ts
 new Env('汪汪赛跑提现')
-Modify By Dylan from HW
 updateTime：2022-07-09
 **/
 
 import {get, post, requireConfig, wait} from './TS_USER_AGENTS'
-import {H5ST} from "./utils/h5st"
+import {H5ST} from "./function/h5st"
 
 let cookie: string = '', res: any = '', UserName: string = '', fp_448de: string = '' || process.env.FP_448DE, fp_b6ac3: string = '' || process.env.FP_B6AC3
 let h5stTool: H5ST = null
@@ -24,7 +23,7 @@ let h5stTool: H5ST = null
       await h5stTool.__genAlgo()
       res = await team('runningMyPrize', {"linkId": "L-sOanK_5RJCz7I314FpnQ", "pageSize": 20, "time": null, "ids": null})
 	  rewardAmount = res.data.rewardAmount
-      if (res.data.runningCashStatus.currentEndTime && res.data.runningCashStatus.status === 0) {
+      if (res.data.runningCashStatus.currentEndTime) {
         console.log('可提现', rewardAmount)
         res = await api('runningPrizeDraw', {"linkId": "L-sOanK_5RJCz7I314FpnQ", "type": 2, "level": 3})
         await wait(2000)
