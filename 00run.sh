39b72207e5e244f2b8ebe5c122d921e1e031318b
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-18 22:42:07 +0800

    Update 00run.sh

--- 00run.sh
+++ 00run.sh
@@ -5,31 +5,31 @@
 ### api.jdsharecode.xyz
 ### sharecodepool.cnmb.win
 
-curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=farm&sharecode=ab20ce7ec4294af6af5f11ce657e6bcb"
-curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=farm&sharecode=2764c0d2f0c44a32906c895b3cca8c66"
-curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=farm&sharecode=0fda5c9ebd194434aaecbbdf0d8741af"
-curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=farm&sharecode=670b6a89e4694fc3be6b79bc93a51c4e"
+#curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=farm&sharecode=ab20ce7ec4294af6af5f11ce657e6bcb"
+#curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=farm&sharecode=2764c0d2f0c44a32906c895b3cca8c66"
+#curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=farm&sharecode=0fda5c9ebd194434aaecbbdf0d8741af"
+#curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=farm&sharecode=670b6a89e4694fc3be6b79bc93a51c4e"
 
-curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=bean&sharecode=4npkonnsy7xi23qpldd3r2j7uacjaa2lz2pdlji"
-curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=bean&sharecode=ik7t7zkjpb2rs6zc7ufixlk6xa"
-curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=bean&sharecode=ll3bia54pjfx47gq5c2rmder7u"
-curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=bean&sharecode=5y5ua2n2xjibrnna4xpkq3gmmq"
+#curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=bean&sharecode=4npkonnsy7xi23qpldd3r2j7uacjaa2lz2pdlji"
+#curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=bean&sharecode=ik7t7zkjpb2rs6zc7ufixlk6xa"
+#curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=bean&sharecode=ll3bia54pjfx47gq5c2rmder7u"
+#curl "https://sharecodepool.cnmb.win/api/runTimes0917?activityId=bean&sharecode=5y5ua2n2xjibrnna4xpkq3gmmq"
 
 ###
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=ab20ce7ec4294af6af5f11ce657e6bcb&type=farm"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=2764c0d2f0c44a32906c895b3cca8c66&type=farm"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=0fda5c9ebd194434aaecbbdf0d8741af&type=farm"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=670b6a89e4694fc3be6b79bc93a51c4e&type=farm"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=ab20ce7ec4294af6af5f11ce657e6bcb&type=farm"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=2764c0d2f0c44a32906c895b3cca8c66&type=farm"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=0fda5c9ebd194434aaecbbdf0d8741af&type=farm"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=670b6a89e4694fc3be6b79bc93a51c4e&type=farm"
 
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=4npkonnsy7xi23qpldd3r2j7uacjaa2lz2pdlji&type=bean"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=ik7t7zkjpb2rs6zc7ufixlk6xa&type=bean"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=ll3bia54pjfx47gq5c2rmder7u&type=bean"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=5y5ua2n2xjibrnna4xpkq3gmmq&type=bean"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=4npkonnsy7xi23qpldd3r2j7uacjaa2lz2pdlji&type=bean"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=ik7t7zkjpb2rs6zc7ufixlk6xa&type=bean"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=ll3bia54pjfx47gq5c2rmder7u&type=bean"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=5y5ua2n2xjibrnna4xpkq3gmmq&type=bean"
 
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=46yqG_ZdJTsdL17EBpfUh4s&type=city&user=starjason"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=6a2zAP1fPjEdIkuIBtuJu_7yvQ&type=city&user=yuxiachenqi"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=9q3-XK4MPD0dNF6ISpeJ5ire&type=city&user=fu5520jing"
-curl "http://www.jdhelp.cf/jdcodes/submit.php?code=-ryUXqwFNWdHakTFSYnA9sux6ZHZflFB&type=city&user=jd_709c349f13b51"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=46yqG_ZdJTsdL17EBpfUh4s&type=city&user=starjason"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=6a2zAP1fPjEdIkuIBtuJu_7yvQ&type=city&user=yuxiachenqi"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=9q3-XK4MPD0dNF6ISpeJ5ire&type=city&user=fu5520jing"
+#curl "http://www.jdhelp.cf/jdcodes/submit.php?code=-ryUXqwFNWdHakTFSYnA9sux6ZHZflFB&type=city&user=jd_709c349f13b51"
 
 
 ### api.jdsharecode.xyz
