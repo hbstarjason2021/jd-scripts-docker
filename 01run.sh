52c63a8f84d1264b08fced8a5edfc49225c496bf
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2024-11-24 23:14:07 +0800

    Update 01run.sh

--- 01run.sh
+++ 01run.sh
@@ -96,6 +96,8 @@ time docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source
 ##jd_bean_home.js
 time docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_bean_home.js'
 
+time docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_bean_home-new.js'
+
 ## 健康社区
 #docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_health-new.js'
 #docker exec jd1 bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; node jd_health-new2.js'
